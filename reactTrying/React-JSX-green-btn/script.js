f55fe7d7396a6fac51a702ac9f57f3364c3be814
Egor <Gungames28.04@gmail.com>
2023-12-22 21:08:40 +0600

    task 27, 28

--- reactTrying/React-JSX-green-btn/script.js
+++ reactTrying/React-JSX-green-btn/script.js
@@ -17,4 +17,25 @@ const container = document.getElementById('app')
 const root = ReactDOM.createRoot (container)
 root.render(<App />)
 
-// 63 Lesson
+// option 2
+
+const App = ({initialButtonText, initialClassesList}) => {
+    const [buttonText, setButtonText] = React.useState(props.initialButtonText)
+    // buttonText = 'Click me', начальное значение
+    // setButtonText - функция, которая меняет начальное значение
+    const [classesList, setClassesList] = React.useState ('initialClassesList')
+    const onButtonClick = () => {
+        setButtonText ('Hello from React')
+        setClassesList ('green-btn')
+    }
+
+
+    return (
+        <div className="app">
+            <button className={classesList} onClick={onButtonClick}>{buttonText}</button>
+        </div>
+    )
+}
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot (container)
+root.render(<App initialButtonText = "Click me" initialClassesList = "" />)
